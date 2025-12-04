4e759058ebdcd643e2228b562617ee9ea53440cf
Firkuss <firkusblaze@gmail.com>
2025-12-04 13:28:02 +0100

    change

--- Assets/Scripts/GoldAutomator.cs
+++ Assets/Scripts/GoldAutomator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
@@ -5,28 +6,40 @@ using UnityEngine;
 public class GoldAutomator : MonoBehaviour
 {
     public GoldManager goldA;
+    public GoldManager goldT;
     public int price;
     public int giver;
     public float time;
+    private bool isCoroutineLaunched;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         time = 1;
-        Givebysecond(false);
     }
 
-    public IEnumerator Givebysecond(bool activate)
+    public IEnumerator Givebysecond()
     {
-        yield return new WaitForSeconds(time);
-        goldA.goldAmount = goldA.goldAmount + giver;
+        isCoroutineLaunched = true;
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            goldA.goldAmount = goldA.goldAmount + giver;
+            goldT.goldText.text = goldA.goldAmount.ToString("00");
+        }
     }
 
     public void ChangeAuto()
     {
         if (goldA.goldAmount >= price)
         {
-            Givebysecond(true);
+            if(isCoroutineLaunched == false)
+            {
+                StartCoroutine( Givebysecond());
+            }
+            goldA.goldAmount -= price;
+            goldT.goldText.text = goldA.goldAmount.ToString("00");
             time = time * 0.5f;
+            price = (int) MathF.Ceiling(price * 1.1f);
         }
     }
     // Update is called once per frame
